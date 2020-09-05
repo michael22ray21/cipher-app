f7c4d651ef5d8fbb5efee63616ce537bc4be6695
michael22ray21 <michael22ray21@yahoo.com>
2020-09-05 13:05:08 +0700

    Init commit

---
+++ crypto.py
@@ -0,0 +1,13 @@
+# Michael Ray
+# Vigenére cipher
+# Full Vigenére cipher
+# Running key Vigenére cipher
+# Super encryption : Vigenére cipher + Tranpositional cipher
+# Afffine cipher
+
+# Stefanus Ardi Mulia
+# Extended Vigenére cipher
+# Playfair cipher
+# Hill cipher
+
+# Bonus : Enigma cipher
