07c9c3563e4bf834e6859231343017ce7e34dc9d
Stefanus Ardi Mulia <stefanus.ardimulia@gmail.com>
2020-09-09 05:33:27 +0700

    Add inquirer

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
+import inquirer
 import HandleFiles as hf
 
 if __name__ == "__main__":
-    pass
+    cipher_question = [
+        inquirer.List(
+            name="cipher_type",
+            message="Choose cipher type",
+            choices=[
+                "Vigenere Cipher",
+                "Full Vigenere Cipher",
+                "Auto-key Vigenere Cipher",
+                "Extended Vigenere Cipher",
+                "Playfair Cipher",
+                "Super Encryption",
+                "Affine Cipher",
+                "Hill Cipher",
+                "Enigma Cipher"
+            ],
+        )
+    ]
+
+    cipher_answer = inquirer.prompt(cipher_question)
+
+    if cipher_answer['cipher_type'] == "Vigenere Cipher":
+        raise NotImplementedError
+    elif cipher_answer['cipher_type'] == "Full Vigenere Cipher":
+        raise NotImplementedError
+    elif cipher_answer['cipher_type'] == "Auto-key Vigenere Cipher":
+        raise NotImplementedError
+    elif cipher_answer['cipher_type'] == "Extended Vigenere Cipher":
+        raise NotImplementedError
+    elif cipher_answer['cipher_type'] == "Playfair Cipher":
+        raise NotImplementedError
+    elif cipher_answer['cipher_type'] == "Super Cipher":
+        raise NotImplementedError
+    elif cipher_answer['cipher_type'] == "Affine Cipher":
+        raise NotImplementedError
+    elif cipher_answer['cipher_type'] == "Hill Cipher":
+        raise NotImplementedError
+    elif cipher_answer['cipher_type'] == "Enigma Cipher":
+        raise NotImplementedError
+    else:
+        raise NotImplementedError
